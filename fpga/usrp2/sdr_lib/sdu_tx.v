d2cff5c78d65df8648cf35cc6c99338cc1cd3956
Eric Wustrow <ewust@umich.edu>
2014-06-03 19:40:20 -0400

    sdu_tx_en and sdu_seq_done_strobe are inputs to sdu_tx

--- fpga/usrp2/sdr_lib/sdu_tx.v
+++ fpga/usrp2/sdr_lib/sdu_tx.v
@@ -4,8 +4,8 @@ module sdu_tx(
 	input reset,
 
 	//SDUltrasound control signals
-	output reg sdu_tx_en,
-	output reg sdu_seq_done_strobe,
+	input reg sdu_tx_en,
+	input reg sdu_seq_done_strobe,
 
 	//PC interface
 	input [15:0] sdu_tx_data,
